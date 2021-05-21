4fc45c01486cda3f343c41dd4474a8b0630c1559
Erwin <ierwinx@gmail.com>
2021-05-20 23:22:25 -0500

    Initial Commit

---
+++ ServiciosWeb/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ServiciosWeb
+//
+//  Created by Erwin on 20/05/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
